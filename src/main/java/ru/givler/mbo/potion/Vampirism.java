dffd562657785c09cb009ed5f0157eb0e70cef42
Givler <Givler228@gmail.com>
2025-04-27 20:06:10 +0300

    add new effect vampirism

---
+++ src/main/java/ru/givler/mbo/potion/Vampirism.java
@@ -0,0 +1,100 @@
+package ru.givler.mbo.potion;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import ru.givler.mbo.main;
+
+public class Vampirism extends Potion {
+
+    private static final ResourceLocation potionIcon = new ResourceLocation(main.MODID, "textures/gui/vampirism_icon.png");
+    public static int potionId; // Статический ID для проверки наличия эффекта
+
+    public Vampirism(int id, boolean isBadEffect, int liquidColour) {
+        super(id, isBadEffect, liquidColour);
+        this.setPotionName("potion.vampirism");
+        potionId = id; // Сохраняем ID для использования в обработчике событий
+        MinecraftForge.EVENT_BUS.register(new VampirismEventHandler()); // Регистрируем обработчик событий
+    }
+
+    @Override
+    public void performEffect(EntityLivingBase entitylivingbase, int strength) {
+        // Основной эффект будет применяться через систему событий Forge
+    }
+
+    @Override
+    public boolean isReady(int duration, int amplifier) {
+        // Этот эффект не нуждается в периодическом вызове performEffect()
+        return false;
+    }
+
+    @Override
+    public void renderInventoryEffect(int x, int y, PotionEffect effect, net.minecraft.client.Minecraft mc) {
+        // Рисуем иконку эффекта в инвентаре
+        mc.renderEngine.bindTexture(potionIcon);
+        this.drawTexturedRect(x + 6, y + 7, 0, 0, 18, 18, 18, 18);
+    }
+
+    // Рисуем иконку на экране
+    public static void drawTexturedRect(int x, int y, int u, int v, int width, int height, int textureWidth, int textureHeight)
+    {
+        float f = 1F / (float)textureWidth;
+        float f1 = 1F / (float)textureHeight;
+        net.minecraft.client.renderer.Tessellator tessellator = net.minecraft.client.renderer.Tessellator.instance;
+        tessellator.startDrawingQuads();
+        tessellator.addVertexWithUV((double)(x), (double)(y + height), 0, (double)((float)(u) * f), (double)((float)(v + height) * f1));
+        tessellator.addVertexWithUV((double)(x + width), (double)(y + height), 0, (double)((float)(u + width) * f), (double)((float)(v + height) * f1));
+        tessellator.addVertexWithUV((double)(x + width), (double)(y), 0, (double)((float)(u + width) * f), (double)((float)(v) * f1));
+        tessellator.addVertexWithUV((double)(x), (double)(y), 0, (double)((float)(u) * f), (double)((float)(v) * f1));
+        tessellator.draw();
+    }
+
+    // Внутренний класс для обработки событий урона
+    public static class VampirismEventHandler {
+
+        @SubscribeEvent
+        public void onLivingHurt(LivingHurtEvent event) {
+            // Проверяем, что источник урона - сущность
+            if (event.source.getEntity() instanceof EntityLivingBase) {
+                EntityLivingBase attacker = (EntityLivingBase) event.source.getEntity();
+
+                // Проверяем наличие эффекта вампиризма у атакующего
+                if (attacker.isPotionActive(potionId)) {
+                    // Получаем уровень эффекта (начиная с 0)
+                    int level = attacker.getActivePotionEffect(Potion.potionTypes[potionId]).getAmplifier();
+
+                    float vampirismPercent = 0.03F + (level * 0.015F);
+
+                    // Рассчитываем, сколько здоровья нужно восстановить
+                    float healAmount = event.ammount * vampirismPercent;
+
+                    // Восстанавливаем здоровье атакующему
+                    if (attacker instanceof EntityPlayer) {
+                        // Для игрока используем метод heal
+                        ((EntityPlayer) attacker).heal(healAmount);
+                    } else {
+                        // Для других сущностей напрямую меняем здоровье
+                        float currentHealth = attacker.getHealth();
+                        attacker.setHealth(Math.min(currentHealth + healAmount, attacker.getMaxHealth()));
+                    }
+
+
+                    if (attacker.worldObj.isRemote && attacker instanceof EntityPlayer) {
+                        for (int i = 0; i < 5; i++) {
+                            attacker.worldObj.spawnParticle("heart",
+                                    attacker.posX + (attacker.getRNG().nextDouble() - 0.5) * 0.5,
+                                    attacker.posY + attacker.getEyeHeight(),
+                                    attacker.posZ + (attacker.getRNG().nextDouble() - 0.5) * 0.5,
+                                    0.0D, 0.1D, 0.0D);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main/java/ru/givler/mbo/registry/PotionRegistry.java
+++ src/main/java/ru/givler/mbo/registry/PotionRegistry.java
@@ -2,13 +2,16 @@ package ru.givler.mbo.registry;
 
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraft.potion.Potion;
-import ru.givler.mbo.potion.DamageBoost;
+import ru.givler.mbo.potion.Vampirism;
+import ru.givler.mbo.potion.MeleeDamage;
 
 public class PotionRegistry {
 
     public static void preLoad(FMLInitializationEvent event) {
-        Potion damageBoostPotion = new DamageBoost(30, false, 0xFF0000);
-        Potion.potionTypes[30] = damageBoostPotion;
+        Potion MeleeDamage = new MeleeDamage(26, false, 0xFF0000);
+        Potion.potionTypes[26] = MeleeDamage;
+        Potion Vampirism = new Vampirism (27, false, 0x800000);
+        Potion.potionTypes[27] = Vampirism;
     }
 }
 
