d39a511ebc556342526837eb75b18ec20ae5e9dd
Givler <Givler228@gmail.com>
2025-05-11 03:01:55 +0300

    fixed bug of totem cleansing

--- src/main/java/ru/givler/mbo/item/totems/ItemTotemCleansing.java
+++ src/main/java/ru/givler/mbo/item/totems/ItemTotemCleansing.java
@@ -1,20 +1,42 @@
 package ru.givler.mbo.item.totems;
 
-        import java.util.ArrayList;
-        import java.util.List;
-        import cpw.mods.fml.common.registry.GameRegistry;
-        import net.minecraft.entity.player.EntityPlayer;
-        import net.minecraft.item.EnumRarity;
-        import net.minecraft.item.Item;
-        import net.minecraft.item.ItemStack;
-        import net.minecraft.potion.Potion;
-        import net.minecraft.potion.PotionEffect;
-        import net.minecraft.world.World;
-        import ru.givler.mbo.EnumParticleType;
-        import ru.givler.mbo.main;
-        import ru.givler.mbo.registry.CreativeTabRegistry;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+import ru.givler.mbo.EnumParticleType;
+import ru.givler.mbo.main;
+import ru.givler.mbo.registry.CreativeTabRegistry;
 
 public class ItemTotemCleansing extends Item {
+
+    /** Рефлексивное поле isBadEffect в Potion */
+    private static Field badEffectField;
+
+    static {
+        try {
+            // Попытка получить human-readable имя
+            badEffectField = Potion.class.getDeclaredField("isBadEffect");
+            badEffectField.setAccessible(true);
+        } catch (NoSuchFieldException e1) {
+            try {
+                // Попытка obf-имени в 1.7.10
+                badEffectField = Potion.class.getDeclaredField("field_76418_K");
+                badEffectField.setAccessible(true);
+            } catch (Exception e2) {
+                badEffectField = null;
+                e2.printStackTrace();
+            }
+        }
+    }
 
     public ItemTotemCleansing(String name, String texture, int maxStackSize) {
         this.canRepair = false;
@@ -29,14 +51,15 @@ public class ItemTotemCleansing extends Item {
 
     @Override
     public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
-        // Убираем все эффекты, кроме положительных
+        // Сервер: удаляем все отрицательные эффекты
         if (!world.isRemote) {
             List<PotionEffect> effectsToRemove = new ArrayList<>();
 
             for (Object obj : player.getActivePotionEffects()) {
                 PotionEffect effect = (PotionEffect) obj;
                 int potionID = effect.getPotionID();
-                if (Potion.potionTypes[potionID].isBadEffect())  {
+                Potion potion = Potion.potionTypes[potionID];
+                if (isBadEffect(potion)) {
                     effectsToRemove.add(effect);
                 }
             }
@@ -46,20 +69,36 @@ public class ItemTotemCleansing extends Item {
             }
             itemStack.damageItem(50, player);
         }
+
+        // Клиент: эффект частиц
         if (world.isRemote) {
             for (int i = 0; i < 30; i++) {
-                main.proxy.spawnParticle(EnumParticleType.SACRED, world,
+                main.proxy.spawnParticle(
+                        EnumParticleType.SACRED, world,
                         player.posX + (world.rand.nextDouble() - 0.5) * 2.0,
                         player.posY + (world.rand.nextDouble() * 0.5) * -1.5,
                         player.posZ + (world.rand.nextDouble() - 0.5) * 2.0,
-                        0.0, 0.1, 0.0);
+                        0.0, 0.1, 0.0
+                );
             }
         }
-        // Проигрываем звук НА СЕРВЕРЕ, чтобы он был слышен всем
+
+        // Звук для всех игроков
         world.playSoundAtEntity(player, "mbo:temple", 1.0F, 1.0F);
         player.swingItem();
         return itemStack;
     }
+
+    /** Проверяет через Reflection приватное поле isBadEffect */
+    private static boolean isBadEffect(Potion potion) {
+        if (badEffectField == null || potion == null) return false;
+        try {
+            return badEffectField.getBoolean(potion);
+        } catch (IllegalAccessException e) {
+            return false;
+        }
+    }
+
     @Override
     public EnumRarity getRarity(ItemStack itemStack) {
         return EnumRarity.epic;
