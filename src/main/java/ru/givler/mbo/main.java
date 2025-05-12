9b23e4e8e484a3ec2c1a8429028e0c8b5dfc16f7
Givler <Givler228@gmail.com>
2025-05-12 15:36:23 +0300

    characteristics update of food

--- src/main/java/ru/givler/mbo/main.java
+++ src/main/java/ru/givler/mbo/main.java
@@ -18,7 +18,7 @@ public class main {
 
     public static final String MODID = "mbo";
     public static final String MODNAME = "MBO";
-    public static final String VERSION = "1.4.5";
+    public static final String VERSION = "1.4.6";
 
     @Mod.EventHandler
     public void preLoad(FMLPreInitializationEvent event) { proxy.preInit(event); }
--- src/main/java/ru/givler/mbo/registry/FoodRegistry.java
+++ src/main/java/ru/givler/mbo/registry/FoodRegistry.java
@@ -6,26 +6,30 @@ import net.minecraft.potion.Potion;
 import ru.givler.mbo.item.ItemsFood;
 
 public class FoodRegistry {
+    /**
+     * Первая характеристика(healAmount) - количество восстановляемого голода.
+     * Вторая(saturation) - коэффициент насыщенности
+     * Чтобы понять сколько насыщенности будет, восспользуйтесь формулой:
+     * healAmount * 2 * saturation  ||  количество восстановляемого голода * 2 * коэффициент насыщенности
+     */
     //переменные для еды
     public static Item FoodBacon, FoodBlackBread, FoodBurger, FoodChowder, FoodDeliciousChicken, FoodDeliciousSalad, FoodDivineSteak, FoodFreshBread,
             FoodFriedSausage, FoodCheese, FoodSoup, FoodMeatPie, FoodStrangeFish;
     public static void preLoad(FMLPreInitializationEvent event) {
-        FoodBacon = new ItemsFood("FoodBacon", "food/bacon", 8, 0.6F, 64, false);
-        FoodBlackBread = new ItemsFood("FoodBlackBread", "food/black_bread", 8, 0.6F, 64, false);
-        FoodBurger = new ItemsFood("FoodBurger", "food/burger", 8, 0.6F, 64, false);
-        FoodChowder = new ItemsFood("FoodChowder", "food/chowder", 8, 0.6F, 1, false);
-        FoodDeliciousChicken = new ItemsFood("FoodDeliciousChicken", "food/delicious_chicken", 8, 0.6F, 64, false);
-        FoodDeliciousSalad = new ItemsFood("FoodDeliciousSalad", "food/delicious_salad", 8, 0.6F, 1, false);
-        FoodDivineSteak = new ItemsFood("FoodDivineSteak", "food/divine_steak", 8, 0.6F, 64, false);
-        FoodFreshBread = new ItemsFood("FoodFreshBread", "food/fresh_bread", 8, 0.6F, 64, false);
-        FoodFriedSausage = new ItemsFood("FoodFriedSausage", "food/fried_sausage", 8, 0.6F, 64, false);
-        FoodCheese = new ItemsFood("FoodCheese", "food/great_cheese", 8, 0.6F, 64, false);
-        FoodSoup = new ItemsFood("FoodSoup", "food/hearty_soup", 8, 0.6F, 1, false);
-        FoodMeatPie = new ItemsFood("FoodMeatPie", "food/meat_pie", 8, 0.6F, 64, false);
+        FoodBacon = new ItemsFood("FoodBacon", "food/bacon", 6, 0.85F, 64, false);
+        FoodBlackBread = new ItemsFood("FoodBlackBread", "food/black_bread", 5, 0.7F, 64, false);
+        FoodBurger = new ItemsFood("FoodBurger", "food/burger", 8, 0.8F, 64, false);
+        FoodChowder = new ItemsFood("FoodChowder", "food/chowder", 7, 0.8F, 1, false);
+        FoodDeliciousChicken = new ItemsFood("FoodDeliciousChicken", "food/delicious_chicken", 6, 0.8F, 64, false);
+        FoodDeliciousSalad = new ItemsFood("FoodDeliciousSalad", "food/delicious_salad", 7, 0.75F, 1, false);
+        FoodDivineSteak = new ItemsFood("FoodDivineSteak", "food/divine_steak", 8, 0.85F, 64, false);
+        FoodFreshBread = new ItemsFood("FoodFreshBread", "food/fresh_bread", 5, 0.7F, 64, false);
+        FoodFriedSausage = new ItemsFood("FoodFriedSausage", "food/fried_sausage", 6, 0.9F, 64, false);
+        FoodCheese = new ItemsFood("FoodCheese", "food/great_cheese", 6, 0.8F, 64, false);
+        FoodSoup = new ItemsFood("FoodSoup", "food/hearty_soup", 7, 0.8F, 1, false);
+        FoodMeatPie = new ItemsFood("FoodMeatPie", "food/meat_pie", 8, 0.8F, 64, false);
 
-        FoodStrangeFish = new ItemsFood("FoodStrangeFish", "food/strange_fried_fish", 8, 0.6F, 1, false)
-                .addPotionEffect(Potion.regeneration.id, 200, 1)  // Эффект регенерации на 200 тиков с усилением 1
-                .addPotionEffect(Potion.moveSpeed.id, 100, 0);  // Эффект ускорения на 100 тиков без усиления
-
+        FoodStrangeFish = new ItemsFood("FoodStrangeFish", "food/strange_fried_fish", 6, 0.8F, 1, false)
+                .addPotionEffect(Potion.regeneration.id, 100, 0);
     }
 }
